3521f2a2b58a18f1ba2aa769a07f14be436af931
popados <popados@gmail.com>
2026-04-10 15:27:24 -0500

    popa/fixed-heatmap

--- src/config/apiBaseUrl.js
+++ src/config/apiBaseUrl.js
@@ -1,5 +1,5 @@
-const PROD_API_BASE_URL = "https://ritoheatmap.info";
+export const API_BASE_URL = "https://ritoheatmap.info";
 
-export const API_BASE_URL =
-	window.__LEAGUE_VIBE_API_BASE_URL__
-	|| (window.location.hostname === "ritoheatmap.info" ? PROD_API_BASE_URL : window.location.origin);
+// export const API_BASE_URL =
+// 	window.__LEAGUE_VIBE_API_BASE_URL__
+// 	|| (window.location.hostname === "https://ritoheatmap.info" ? PROD_API_BASE_URL : window.location.origin);
